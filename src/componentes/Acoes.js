d92464a6df4215736a16061c175d831e26769b5c
goulartggoliveira <gustavoggoliveira98@gmail.com>
2022-07-18 16:32:25 -0300

    Projeto Finalizado

--- src/componentes/Acoes.js
+++ src/componentes/Acoes.js
@@ -1,8 +1,25 @@
-function Acoes() {
+import React from "react";
+
+function Acoes(Props) {
+  function changeIcon() {
+    if (Props.iconName === "heart-outline") {
+      Props.setIconName("heart");
+      Props.setIconClass("heart-color md hydrated");
+    }
+    if (Props.iconName === "heart") {
+      Props.setIconName("heart-outline");
+      Props.setIconClass(" md hydrated");
+    }
+  }
+
   return (
     <div class="acoes">
       <div>
-        <ion-icon name="heart-outline"></ion-icon>
+        <ion-icon
+          name={Props.iconName}
+          class={Props.iconClass}
+          onClick={changeIcon}
+        ></ion-icon>
         <ion-icon name="chatbubble-outline"></ion-icon>
         <ion-icon name="paper-plane-outline"></ion-icon>
       </div>
--- src/componentes/Posts.js
+++ src/componentes/Posts.js
@@ -1,56 +1,73 @@
 import Acoes from "./Acoes";
+import React from "react";
 
 //Objetos dos posts
-let postsDescrip = [
-  {
-    img: "assets/img/meowed.svg",
-    name: "meowed",
-    post: "assets/img/gato-telefone.svg",
-    likeimg: "assets/img/respondeai.svg",
-    userlike: "respondeai",
-    likes: "101.523",
-  },
-  {
-    img: "assets/img/barked.svg",
-    name: "barked",
-    post: "assets/img/dog.svg",
-    likeimg: "assets/img/adorable_animals.svg",
-    userlike: "adorable_animals",
-    likes: "99.159",
-  },
-];
 
-let createPost = postsDescrip.map((post) => (
-  <div class="post">
-    <div class="topo">
-      <div class="usuario">
-        <img src={post.img} />
-        {post.name}
+function Posts() {
+  const [iconName, setIconName] = React.useState("heart-outline");
+  const [iconClass, setIconClass] = React.useState(" md hydrated");
+
+  let postsDescrip = [
+    {
+      img: "assets/img/meowed.svg",
+      name: "meowed",
+      post: "assets/img/gato-telefone.svg",
+      likeimg: "assets/img/respondeai.svg",
+      userlike: "respondeai",
+      likes: "101.523",
+    },
+    {
+      img: "assets/img/barked.svg",
+      name: "barked",
+      post: "assets/img/dog.svg",
+      likeimg: "assets/img/adorable_animals.svg",
+      userlike: "adorable_animals",
+      likes: "99.159",
+    },
+  ];
+
+  let createPost = postsDescrip.map((post) => (
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={post.img} />
+          {post.name}
+        </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
       </div>
-      <div class="acoes">
-        <ion-icon name="ellipsis-horizontal"></ion-icon>
+
+      <div class="conteudo">
+        <img src={post.post} onClick={changeIcon} />
       </div>
-    </div>
 
-    <div class="conteudo">
-      <img src={post.post} />
-    </div>
+      <div class="fundo">
+        <Acoes
+          iconClass={iconClass}
+          iconName={iconName}
+          setIconClass={setIconClass}
+          setIconName={setIconName}
+        />
 
-    <div class="fundo">
-      <Acoes />
-
-      <div class="curtidas">
-        <img src={post.likeimg} />
-        <div class="texto">
-          Curtido por <strong>{post.userlike}</strong> e
-          <strong>outras {post.likes} pessoas</strong>
+        <div class="curtidas">
+          <img src={post.likeimg} />
+          <div class="texto">
+            Curtido por <strong>{post.userlike}</strong> e
+            <strong>outras {post.likes} pessoas</strong>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-));
+  ));
 
-function Posts() {
+  function changeIcon() {
+    if (iconName === "heart-outline") {
+      setIconName("heart");
+      setIconClass("heart-color md hydrated");
+    }
+  }
+
   return <div class="posts">{createPost}</div>;
 }
 export default Posts;
